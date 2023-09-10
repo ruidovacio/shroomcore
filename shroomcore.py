d6159409914602ff0b7f96d4bc53c90349b70cfe
ruidovacio <nikkolazhito@hotmail.com>
2023-09-10 20:01:22 -0300

    fixes

--- shroomcore.py
+++ shroomcore.py
@@ -10,33 +10,30 @@ root = Tk()
 root.title("shroomcore")
 root.geometry("500x550")
 
-def fetch_urls(url): #funcion que hace fetch del api
+
+def fetch_urls(url):  # funcion que hace fetch del api
     print("cargando...")
     response = requests.get(url)
     if response.status_code == 200:
         print("todo ok")
         fetched_lista = response.json()
         print(f"cargada lista con {len(fetched_lista)} valores")
-        for x in fetched_lista:
-            if x["img"] == "":
-                fetched_lista.remove(x)
-        for x in fetched_lista:
-            if x["commonname"] == "":
-                fetched_lista.remove(x)
-        for x in fetched_lista:
-            if x["agent"] == "":
-                fetched_lista.remove(x)
-        print(f"clean up con {len(fetched_lista)} valores")
-        return fetched_lista
+        filtered_lista = [item for item in fetched_lista if (item.get("img"))]
+        filtered_lista = [item for item in filtered_lista if (item.get("commonname"))]
+        filtered_lista = [item for item in filtered_lista if (item.get("agent"))]
+        print(f"clean up con {len(filtered_lista)} valores")
+        return filtered_lista
     else:
         print(response.status_code)
 
 
-def change_index(change): #funcion de los botones
+def change_index(change):  # funcion de los botones
     global index
+    global clicked_button
+    clicked_button = change
     if (index+change >= len(shrooms)):
         index = 0
-    elif (index+change <= 0):
+    elif (index+change < 0):
         index = len(shrooms)-1
     else:
         index += change
@@ -46,25 +43,26 @@ def change_index(change): #funcion de los botones
 def cargar_imagen():
     global index
     img_url = shrooms[index]["img"]
-    print(img_url)
     encoded_url = quote(img_url, safe='/:?=&')
-    img_response = requests.get(
-        encoded_url, headers={'User-Agent': 'Mozilla/5.0'})
-    print(img_response)
-    img_data = img_response.content
-    img_cargar = Image.open(io.BytesIO(img_data))
-    img_cargar = img_cargar.resize((200, 200), Image.NEAREST).convert(
-        'P', palette=Image.ADAPTIVE, colors=16).resize((400, 400), Image.NEAREST)
-    img_foto = ImageTk.PhotoImage(img_cargar)
-    display.config(image=img_foto)
-    display.image = img_foto
+    try:
+        img_response = requests.get(
+            encoded_url, headers={'User-Agent': 'Mozilla/5.0'})
+        print(img_response)
+        img_cargar = Image.open(io.BytesIO(img_response.content))
+        img_filtros = img_cargar.resize((200, 200), Image.NEAREST).convert(
+            'P', palette=Image.ADAPTIVE, colors=16).resize((400, 400), Image.NEAREST)
+        img_done = ImageTk.PhotoImage(img_filtros)
+        display.config(image=img_done)
+        display.image = img_done
+    except:
+        print("error cargando imagen, cambiando...")
+        change_index(clicked_button)
 
-    # display.config(image=shrooms[index]["img"])
-    # print(shrooms[index]["img"])
 
-
-def update(): #actualizar info
-    print(index)
+def update():  # actualizar info
+    print("-------------")
+    print(f"cargando indice {index}")
+    # print(shrooms[index])
     nombre.config(text=shrooms[index]["commonname"])
     descripcion.config(
         text=f'{shrooms[index]["name"]} \n {shrooms[index]["agent"]} \n {shrooms[index]["type"]}')
