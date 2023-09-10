da1309f13eb404417338f0d946b68d70dec22711
ruidovacio <nikkolazhito@hotmail.com>
2023-09-10 19:01:01 -0300

    can display images now

--- shroomcore.py
+++ shroomcore.py
@@ -1,12 +1,16 @@
 from tkinter import *
 import requests
 import json
+from PIL import Image, ImageTk
+import io
+from urllib.parse import quote
 
 URL = "https://toxicshrooms.vercel.app/api/mushrooms"
 root = Tk()
 root.title("shroomcore")
+root.geometry("500x550")
 
-def fetch_urls(url):
+def fetch_urls(url): #funcion que hace fetch del api
     print("cargando...")
     response = requests.get(url)
     if response.status_code == 200:
@@ -28,15 +32,7 @@ def fetch_urls(url):
         print(response.status_code)
 
 
-shrooms = fetch_urls(URL)
-index = 0
-
-nombre = Label(root, font=("Arial 15 bold"),text=shrooms[index]["commonname"])
-nombre.grid(column=1, row=0)
-descripcion = Label(root, text=f'{shrooms[index]["name"]} \n {shrooms[index]["agent"]} \n {shrooms[index]["type"]}')
-descripcion.grid(column=1, row=1)
-
-def change_index(change):
+def change_index(change): #funcion de los botones
     global index
     if (index+change >= len(shrooms)):
         index = 0
@@ -46,15 +42,53 @@ def change_index(change):
         index += change
     update()
 
-def update():
-    print(shrooms[index])
-    nombre.config(text=shrooms[index]["commonname"])
-    descripcion.config(text=f'{shrooms[index]["name"]} \n {shrooms[index]["agent"]} \n {shrooms[index]["type"]}')
 
+def cargar_imagen():
+    global index
+    img_url = shrooms[index]["img"]
+    print(img_url)
+    encoded_url = quote(img_url, safe='/:?=&')
+    img_response = requests.get(
+        encoded_url, headers={'User-Agent': 'Mozilla/5.0'})
+    print(img_response)
+    img_data = img_response.content
+    img_cargar = Image.open(io.BytesIO(img_data))
+    img_cargar = img_cargar.resize((200, 200), Image.NEAREST).convert(
+        'P', palette=Image.ADAPTIVE, colors=16).resize((400, 400), Image.NEAREST)
+    img_foto = ImageTk.PhotoImage(img_cargar)
+    display.config(image=img_foto)
+    display.image = img_foto
+
+    # display.config(image=shrooms[index]["img"])
+    # print(shrooms[index]["img"])
+
+
+def update(): #actualizar info
+    print(index)
+    nombre.config(text=shrooms[index]["commonname"])
+    descripcion.config(
+        text=f'{shrooms[index]["name"]} \n {shrooms[index]["agent"]} \n {shrooms[index]["type"]}')
+    cargar_imagen()
+
+
+# cargar la lista
+shrooms = fetch_urls(URL)
+index = 0
+
+# cargar el gui
+nombre = Label(root, font=("Arial 15 bold"), text=None)
+nombre.grid(column=1, row=0)
+descripcion = Label(
+    root, text=None)
+descripcion.grid(column=1, row=1)
+
+display = Label(root, image=None)
+display.grid(column=1, row=2)
 
 b_prev = Button(root, text="<<", command=lambda: change_index(-1)
-                ).grid(column=0, row=2)
+                ).grid(column=0, row=3)
 b_next = Button(root, text=">>", command=lambda: change_index(1)
-                ).grid(column=2, row=2)
+                ).grid(column=2, row=3)
 
+update()
 root.mainloop()
